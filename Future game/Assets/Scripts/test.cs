2dbf433634bd517e90236e7d15073b025d412df1
LauFRay <LauFRay@users.noreply.github.com>
2018-04-02 17:16:14 +0600

    new krushka

--- Future game/Assets/Scripts/test.cs
+++ Future game/Assets/Scripts/test.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class test : MonoBehaviour {
 
-	// Use this for initialization
+
+    public KeyCode key;
+   [SerializeField] private GameObject game;
+
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        RaycastHit hit;
+        if (Input.GetKeyDown(key))
+        {
+            if (Physics.Raycast(game.transform.position, Vector3.forward,out hit,20f, 1 << 9))
+            {
+                Debug.Log(hit.point);
+            }
+        }
+
 	}
 }
