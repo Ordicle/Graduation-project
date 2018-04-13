2df7b0a2a5cd80a7b88089ef5092a6e20489aca2
LauFRay <LauFRay@users.noreply.github.com>
2018-04-13 18:25:39 +0600

    gggg

--- Future game/Assets/Scripts/test.cs
+++ Future game/Assets/Scripts/test.cs
@@ -2,32 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour {
+public class test : MonoBehaviour
+{
 
 
 
     public KeyCode key;
-   [SerializeField] private GameObject game;
+    public KeyCode tp;
+    [SerializeField] private GameObject game;
 
 
-	void Start () {
-      
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
         RaycastHit hit;
         if (Input.GetKey(key))
         {
-            //if (Physics.Raycast(game.transform.position, game.transform.forward ,out hit,20f, 1 << 9))
-            //{
-            //    hit.rigidbody.transform.Rotate(45, 10, 0);         
-            //}
             if (Physics.Raycast(game.transform.position, game.transform.forward, out hit, 20f))
             {
-                hit.rigidbody.transform.position = new Vector3(45, 10, 0);
+                hit.rigidbody.transform.position = new Vector3(55, 100, 52);
             }
-            }
+        }
+        if (Input.GetKey(tp))
+        {
+            game.transform.position = new Vector3(55, 100, 52);
+        }
     }
 }
