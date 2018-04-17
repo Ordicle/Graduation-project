55fba0024a2de51686d6a8eb351ae09eba7c7dd6
LauFRay <LauFRay@users.noreply.github.com>
2018-04-17 11:07:30 +0600

    пппп

--- Future game/Assets/Scripts/test.cs
+++ Future game/Assets/Scripts/test.cs
@@ -10,11 +10,13 @@ public class test : MonoBehaviour
     public KeyCode key;
     public KeyCode tp;
     [SerializeField] private GameObject game;
+    [Range(0,10)]
+    public int up = 10;
 
 
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -26,12 +28,12 @@ public class test : MonoBehaviour
         {
             if (Physics.Raycast(game.transform.position, game.transform.forward, out hit, 20f))
             {
-                hit.rigidbody.transform.position = new Vector3(55, 100, 52);
+                hit.rigidbody.transform.position = new Vector3(hit.rigidbody.transform.position.x, hit.rigidbody.transform.position.y + up, hit.rigidbody.transform.position.z);
             }
         }
         if (Input.GetKey(tp))
         {
-            game.transform.position = new Vector3(55, 100, 52);
+            game.transform.position = new Vector3(game.transform.position.x, game.transform.position.y +up, game.transform.position.z);
         }
     }
 }
